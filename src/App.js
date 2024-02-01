81464520dde9aa7037f7c483754fa9e93219c069
Brent Ahrens <cidida@gmail.com>
2024-02-01 12:47:42 -0800

    create scaffold for the signin page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from "./routes/home/home.component";
+import Shop from './routes/shop/shop.component';
+import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/signin/signin.component';
 
 const App = () => {
-
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/signin' element={<SignIn />} />
+      </Route>
     </Routes>
   );
 }
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,29 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+import logo from '../../assets/basketball.png';
+import './navigation.styles.scss';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="nav">
+        <div className="nav-links-container">
+          <Link className="nav-link" to='/'>
+            <img className="nav-logo" src={logo} alt="Wozzie Logo" />
+          </Link>
+          <div className="nav-link-list">
+            <Link className="nav-link" to='/shop'>
+              Shop
+            </Link>
+            <Link className="nav-link" to='/signin'>
+              Sign in
+            </Link>
+          </div>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  )
+}
+
+export default Navigation;
